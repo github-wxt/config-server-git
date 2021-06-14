522e7f6e1c08c7444f5d864b978b2e7372bd0d66
github-wxt <675632387@qq.com>
2021-06-14 21:01:06 +0800

    上海灿提交

--- src/main/java/com/wxt/configservergit/ConfigServerGitApplication.java
+++ src/main/java/com/wxt/configservergit/ConfigServerGitApplication.java
@@ -1,5 +1,10 @@
 package com.wxt.configservergit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * 〈类描述〉
  * 〈〉
@@ -8,5 +13,12 @@ package com.wxt.configservergit;
  * @create 2021/6/7
  * @since 1.0.0
  */
+
+@EnableConfigServer
+@SpringBootApplication
 public class ConfigServerGitApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(ConfigServerGitApplication.class,args);
+        new SpringApplicationBuilder(ConfigServerGitApplication.class).web(true).run(args);
+    }
 }
